5b5ab7f91fd10392eff0f720ab1e9d799784af7a
Ben Jones <fetiddius@gmail.com>
2025-03-12 21:43:45 +0000

    improve tab controls

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -2,10 +2,24 @@ return {
   'romgrk/barbar.nvim',
   dependencies = {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-    -- 'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
   init = function()
     vim.g.barbar_auto_setup = false
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+    map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+    map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+    map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+    vim.api.nvim_create_augroup('neotree', {})
+    vim.api.nvim_create_autocmd('UiEnter', {
+      group = 'neotree',
+      callback = function()
+        if vim.fn.argc() == 0 then
+          vim.cmd 'Neotree current'
+        end
+      end,
+    })
   end,
   opts = {
     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
