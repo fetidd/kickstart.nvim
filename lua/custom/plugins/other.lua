a23d168ce3abd69287d88dc89998b769646e16cf
Ben Jones <fetiddius@gmail.com>
2025-03-13 10:13:18 +0000

    impropve other

--- lua/custom/plugins/other.lua
+++ lua/custom/plugins/other.lua
@@ -8,6 +8,10 @@ return {
         pattern = '/usr/local/ST/V2/code/(.*)/(.*).py',
         target = '/usr/local/ST/notlive/unittest/%1/Test_%2.py',
       },
+      {
+        pattern = '/usr/local/ST/notlive/unittest/(.*)/Test_(.*).py',
+        target = '/usr/local/ST/V2/code/%1/%2.py',
+      },
     },
   },
 }
